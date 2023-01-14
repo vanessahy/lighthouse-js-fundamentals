8258bc6bf5f1a8235f1f5a032fe73875993e39e4
Vanessa Yim <vanessa.violin38@gmail.com>
2023-01-13 20:55:14 -0500

    using return in a function

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
-const sayHello = function (name) {
-  console.log("Hello, " + name);
+const returnSayHello = function (name) {
+  return "Hello, " + name;
 }
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+const greeting = returnSayHello("John");
+console.log(greeting);
